d763d13710104ac3e3b770d274453b4e278fee32
laushinenjoelle-nm2207 <125462098+laushinenjoelle-nm2207@users.noreply.github.com>
2023-03-30 14:02:21 +0800

    week 10 commit

--- 3exploreSugar/appscript.js
+++ 3exploreSugar/appscript.js
@@ -27,14 +27,17 @@ function changeImage3() {
 }
 
 function changeText(){
-    document.getElementById("text").innerHTML= "Did you know? Your Kaya toast can contain up 2 teaspoons of sugar!"
+    var image = document.getElementById('text');
+        image.src = "resources/kayaText.svg";
 }
 function changeText2(){
-    document.getElementById("text").innerHTML= "Did you know? Your MeeSiam can contain up 7.5 teaspoons of sugar!"
+    var image = document.getElementById('text');
+        image.src = "resources/noodleText.svg";
 }
 
 function changeText3(){
-    document.getElementById("text").innerHTML= "Did you know? A medium-sized 500ml bubble milk tea with pearls and the full amount of sugar has 8 teaspoons of sugar! So be careful when getting your daily bubble tea fix!"
+    var image = document.getElementById('text');
+        image.src = "resources/bbtText.svg";
 }
 
 //function should replace the background image of the food box to kaya when the button is clicked 
--- 5jumpingGame/script.js
+++ 5jumpingGame/script.js
@@ -33,11 +33,26 @@ function checkScore() {
     }   
 }
 
+function levelClear() {
+    finalScore= score.innerHTML;
+    console.log(finalScore);
+    if (finalScore>300){
+        //so that reload will go back to the starting page 
+        alert("you got a score of: "+ score.innerText + ". Level Cleared!");
+        window.location.replace("index_end.html") 
+}
+}
+
 //main game loop
 setInterval(() => {
     sugar.classList.add('sugar-animation');
+    //power.classList.add('power-animation');
     //score increases w time of player survival
     score.innerText++;
+    console.log(score.innerText);
+    if (score.innerText>300){
+        levelClear();
+    }
     //calculating the position of the pancreas and position of the sugar 
     const pancreasTop = parseInt(window.getComputedStyle(pancreas)
     .getPropertyValue('top'));
@@ -51,7 +66,7 @@ setInterval(() => {
         sugar.style.display = ''
     }
     //runs the checkscore function when game ends aka when pancreas hits sugar 
-    if(sugarLeft < 70 && sugarLeft >0 && pancreasTop>145 ){
+    if(sugarLeft < 100 && sugarLeft >0 && pancreasTop> 180 ){
         checkScore();}
     
 },50);
